7076a3db8aec55920682e4a16bf99a584857a343
j <zforyou@naver.com>
2016-08-24 21:33:39 +0900

    - main menu item

--- 01_menu/app/src/main/java/com/example/b/a01_menu/MainActivity.java
+++ 01_menu/app/src/main/java/com/example/b/a01_menu/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.b.a01_menu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.menu_test1){
+            Toast.makeText(MainActivity.this, "menu 1 selected", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
