3550bd29d80a5800d791f5895ebcd3dab906e9eb
j <zforyou@naver.com>
2016-08-29 19:49:34 +0900

    second

--- 06_thread/app/src/main/java/com/example/b/a06_thread/MainActivity.java
+++ 06_thread/app/src/main/java/com/example/b/a06_thread/MainActivity.java
@@ -1,54 +1,59 @@
 package com.example.b.a06_thread;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MYTHREAD = 100;
     Button btn;
-//    class MyThread extends Thread{
-//        @Override
-//        public void run() {
-//            for(int i=0; i<100; i++) {
-//
-//                try {
-//                    Thread.sleep(100);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//                Log.d("MyThread", "count :" +i);
-//                btn.setText("count :" +i);
-//            }
-//        }
-//    }
+    ProgressBar progressBar;
+    class MyThread extends Thread{
+        @Override
+        public void run() {
+            for(int i=0; i<100; i++){
+                try {
+                    sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Log.d("MyThread", "count : "+i);
+//                btn.setText("count : "+i);
+                Message msg = handler.obtainMessage(); //new Message();
+                msg.what = MYTHREAD;
+                msg.arg1 = i;
+                handler.sendMessage(msg);
+            }
+        }
+    }
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if(msg.what == MYTHREAD){
+                btn.setText("count : "+msg.arg1);
+                progressBar.setProgress(msg.arg1);
+            }
+
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = (Button) findViewById(R.id.btn);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
     }
 
+
     public void onBtnClick(View v){
-//        MyThread th = new MyThread();
-//        th.start();
-
-        Thread th2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for(int i=0; i<100; i++) {
-
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    Log.d("MyThread", "count :" +i);
-                    btn.setText("count :" +i);
-                }
-            }
-        });
-        th2.start();
+        MyThread th = new MyThread();
+        th.start();
     }
 }
