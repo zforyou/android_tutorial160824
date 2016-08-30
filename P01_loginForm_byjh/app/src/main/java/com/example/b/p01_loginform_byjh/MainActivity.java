556271cd9f59e06a1f0b087207f21cced86321fe
j <zforyou@naver.com>
2016-08-30 20:02:33 +0900

    sec by HS

--- P01_loginForm_byjh/app/src/main/java/com/example/b/p01_loginform_byjh/MainActivity.java
+++ P01_loginForm_byjh/app/src/main/java/com/example/b/p01_loginform_byjh/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.b.p01_loginform_byjh;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences pref = getSharedPreferences("Login",0);
+
+        final EditText strID = (EditText)findViewById(R.id.editID);
+        final EditText strPW = (EditText)findViewById(R.id.editPW);
+
+        Button btn = (Button)findViewById(R.id.myBtn);
+
+
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("id", String.valueOf(strID));
+        editor.putString("pw", String.valueOf(strPW));
+
+        editor.commit();
+
     }
 }
