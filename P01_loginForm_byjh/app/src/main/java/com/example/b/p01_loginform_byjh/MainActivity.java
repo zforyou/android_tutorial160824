4700f29766eed5f8e622fd01bf7f38436beecd61
j <zforyou@naver.com>
2016-08-30 20:27:02 +0900

    -final (add handle data)

--- P01_loginForm_byjh/app/src/main/java/com/example/b/p01_loginform_byjh/MainActivity.java
+++ P01_loginForm_byjh/app/src/main/java/com/example/b/p01_loginform_byjh/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.b.p01_loginform_byjh;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -26,10 +27,27 @@ public class MainActivity extends AppCompatActivity {
 //
 //        editor.commit();
         editId = (EditText) findViewById(R.id.editID);
+
+        SharedPreferences pref = getSharedPreferences("settings",0);
+
+        // data compare
+        String strId = pref.getString("id", "");
+        if(strId.equals("") == false){
+            Intent intent = new Intent(MainActivity.this, Myactivity.class);
+            intent.putExtra("id", strId);
+            startActivity(intent);
+        }
     }
 
     public void onLoginClick(View v){
         String strId = editId.getText().toString();
+
+        // set data
+        SharedPreferences pref = getSharedPreferences("settings",0);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("id", strId);
+        editor.commit();
+
         Intent intent = new Intent(MainActivity.this, Myactivity.class);
         intent.putExtra("id", strId);
         startActivity(intent);
