018cf4caeab4d118e1799d557d50a6085514432d
j <zforyou@naver.com>
2016-08-30 19:26:29 +0900

    first

--- P01_loginForm/app/src/main/java/com/example/b/p01_loginform/MainActivity.java
+++ P01_loginForm/app/src/main/java/com/example/b/p01_loginform/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,10 +17,12 @@ public class MainActivity extends AppCompatActivity {
         btnNewActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Myactivity.class);
-                intent.putExtra("id", "jsjs550");
-                intent.putExtra("pw","1234567890");
-//                startActivity(intent);
+                Intent intent = new Intent(MainActivity.this, MyloginForm.class);
+                String strID = String.valueOf(findViewById(R.id.editID));
+                intent.putExtra("id", strID);
+                String strPW = String.valueOf(findViewById(R.id.editPW));
+                intent.putExtra("pw", strPW);
+                startActivity(intent);
                 startActivityForResult(intent, MY_REQ_NUM);
             }
         });
@@ -32,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == MY_REQ_NUM){
             if(resultCode == RESULT_OK){
-                String strResult = data.getStringExtra("myResult");
-                Toast.makeText(MainActivity.this, "myResult :"+strResult, Toast.LENGTH_SHORT).show();
+//                String strResult = data.getStringExtra("myResult");
+//                Toast.makeText(MainActivity.this, "myResult :"+strResult, Toast.LENGTH_SHORT).show();
             }
         }
     }
--- P01_loginForm/app/src/main/java/com/example/b/p01_loginform/MyloginForm.java
+++ P01_loginForm/app/src/main/java/com/example/b/p01_loginform/MyloginForm.java
@@ -3,8 +3,6 @@ package com.example.b.p01_loginform;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 /**
@@ -15,24 +13,23 @@ public class MyloginForm extends AppCompatActivity {
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_myactivity);
+            setContentView(R.layout.activity_mylogin);
 
             Intent intent = getIntent();
-            String str = intent.getStringExtra("id").toString();
+            String strID = intent.getStringExtra("id").toString();
 
-            Toast.makeText(Myactivity.this, "id :"+str, Toast.LENGTH_SHORT).show();
-            Toast.makeText(Myactivity.this, "pw :"+intent.getExtras().get("pw").toString(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(MyloginForm.this, "id :"+strID, Toast.LENGTH_SHORT).show();
+//            Toast.makeText(Myactivity.this, "pw :"+intent.getExtras().get("pw").toString(), Toast.LENGTH_SHORT).show();
 
-            Button btnFinish = (Button)findViewById(R.id.btnFinish);
-            btnFinish.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent resultIntent = new Intent();
-                    resultIntent.putExtra("myResult", "abcdef");
-                    setResult(RESULT_OK, resultIntent);
-                    finish();
-                }
-            });
+//            Button btnFinish = (Button)findViewById(R.id.btnFinish);
+//            btnFinish.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    Intent resultIntent = new Intent();
+//                    resultIntent.putExtra("myResult", "abcdef");
+//                    setResult(RESULT_OK, resultIntent);
+//                    finish();
+//                }
+//            });
         }
-    }
 }
