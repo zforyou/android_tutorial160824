01f714f2eae9e6358772de8f05fd6cc542d150b8
j <zforyou@naver.com>
2016-09-01 21:27:56 +0900

    - final

--- 12_sqlite/app/src/main/java/com/example/b/a12_sqlite/MainActivity.java
+++ 12_sqlite/app/src/main/java/com/example/b/a12_sqlite/MainActivity.java
@@ -9,5 +9,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TestDBHandler testDBHandler = new TestDBHandler(this);
+        testDBHandler.insert("kim", "seoul", 20);
+        testDBHandler.insert("가나다", "인천", 21);
+        testDBHandler.insert("john", "LA", 22);
+        testDBHandler.insert("park", "부산", 23);
+
+        testDBHandler.delete("kim");
+        testDBHandler.update("john", 33);
+
+        testDBHandler.showAllData();
     }
 }
