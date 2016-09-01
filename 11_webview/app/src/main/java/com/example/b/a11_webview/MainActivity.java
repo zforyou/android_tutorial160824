658f5ac175d9235bc73ac7846ab983194f04f562
j <zforyou@naver.com>
2016-09-01 19:56:45 +0900

    - second add WebChromeClient

--- 11_webview/app/src/main/java/com/example/b/a11_webview/MainActivity.java
+++ 11_webview/app/src/main/java/com/example/b/a11_webview/MainActivity.java
@@ -5,26 +5,40 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
     WebView webView;
     ProgressDialog dlg;
+    ProgressBar progressBar;
+
     class MyWebViewClient extends WebViewClient{
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
             super.onPageStarted(view, url, favicon);
             dlg.show();
+            progressBar.setVisibility(View.VISIBLE);
         }
 
         @Override
         public void onPageFinished(WebView view, String url) {
             super.onPageFinished(view, url);
             dlg.dismiss();
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+
+    class MyWebChromeClient extends WebChromeClient{
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+            progressBar.setProgress(newProgress);
         }
     }
     @Override
@@ -32,10 +46,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        progressBar = (ProgressBar) findViewById(R.id.loadingprogressBar);
+        dlg = new ProgressDialog(this);
+
         webView = (WebView) findViewById(R.id.webView);
         webView.setWebViewClient(new MyWebViewClient());
+        webView.setWebChromeClient(new MyWebChromeClient());
 
-        dlg = new ProgressDialog(this);
 
         WebSettings ws = webView.getSettings();
         ws.setJavaScriptEnabled(true);
