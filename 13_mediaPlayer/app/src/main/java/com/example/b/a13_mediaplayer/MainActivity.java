7670b98a4d6c39062178357a84f803aa4e4b1518
j <zforyou@naver.com>
2016-09-02 20:23:24 +0900

    - add seekbar and thread todo handler

--- 13_mediaPlayer/app/src/main/java/com/example/b/a13_mediaplayer/MainActivity.java
+++ 13_mediaPlayer/app/src/main/java/com/example/b/a13_mediaplayer/MainActivity.java
@@ -5,19 +5,26 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.SeekBar;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mp = null;
+    SeekBar seekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        seekBar = (SeekBar) findViewById(R.id.seekbar);
     }
 
     public void onPlayClick(View v){
+        if(mp != null) {
+            mp.stop();
+            mp.release();
+        }
         String path = Environment.getExternalStorageDirectory().toString();
         path += "/Genie/music/Water Under The Bridge_Adele.mp3";
 //        path += "/Download/Kalimba.mp3";
@@ -30,6 +37,22 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        seekBar.setMax(mp.getDuration());
+
+        Thread th = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                int position = mp.getCurrentPosition();
+
+            }
+        });
+        th.start();
     }
 
     public void onStopClick(View v){
