f1a09bc1f6f00eaeb6639c69bc88c32477d38e38
j <zforyou@naver.com>
2016-09-05 21:50:06 +0900

    -first

---
+++ 15_br2/app/src/main/java/com/example/b/a15_br2/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.b.a15_br2;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            Toast.makeText(context, "batt : "+level, Toast.LENGTH_LONG).show();
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(receiver, filter);
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        unregisterReceiver(receiver);
+    }
+}
+
+
