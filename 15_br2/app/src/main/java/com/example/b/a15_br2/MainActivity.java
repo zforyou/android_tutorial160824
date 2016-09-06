8c69b9da834f67d8d6bd3ebf163f8472a78d6864
j <zforyou@naver.com>
2016-09-06 19:29:14 +0900

    - add custom broadcast receiver

--- 15_br2/app/src/main/java/com/example/b/a15_br2/MainActivity.java
+++ 15_br2/app/src/main/java/com/example/b/a15_br2/MainActivity.java
@@ -5,8 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,8 +15,15 @@ public class MainActivity extends AppCompatActivity {
     BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
-            Toast.makeText(context, "batt : "+level, Toast.LENGTH_LONG).show();
+            String action = intent.getAction();
+            if(action.equals(Intent.ACTION_BATTERY_CHANGED)){
+                int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+                Toast.makeText(context, "batt : " + level, Toast.LENGTH_LONG).show();
+            }else if(action.equals(Intent.ACTION_BATTERY_LOW)){
+                //
+            }else if(action.equals("abcdef")){
+                Toast.makeText(context, "custom action", Toast.LENGTH_LONG).show();
+            }
         }
     };
     @Override
@@ -29,12 +37,19 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        filter.addAction(Intent.ACTION_BATTERY_LOW);
+        filter.addAction("abcdef");
         registerReceiver(receiver, filter);
     }
     @Override
     protected void onStop(){
         super.onStop();
         unregisterReceiver(receiver);
+    }
+
+    public void onSendClick(View v){
+        Intent intent = new Intent("abcdef");
+        sendBroadcast(intent);
     }
 }
 
