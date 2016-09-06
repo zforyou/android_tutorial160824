792e5072131926b1bda124ddcc2bfdaef68b514a
j <zforyou@naver.com>
2016-09-06 21:00:05 +0900

    - add intent service

---
+++ 17_service2/app/src/main/java/com/example/b/a17_service2/MyIntentService.java
@@ -0,0 +1,17 @@
+package com.example.b.a17_service2;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+
+public class MyIntentService extends IntentService {
+
+    public MyIntentService() {
+        super("MyIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
