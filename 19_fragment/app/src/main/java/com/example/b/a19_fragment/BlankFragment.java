027b6cef2bc4165a183d16301a087c9725d91545
j <zforyou@naver.com>
2016-09-07 19:51:04 +0900

    - add fragment

--- 19_fragment/app/src/main/java/com/example/b/a19_fragment/BlankFragment.java
+++ 19_fragment/app/src/main/java/com/example/b/a19_fragment/BlankFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
@@ -23,7 +25,18 @@ public class BlankFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        View v =  inflater.inflate(R.layout.fragment_blank, container, false);
+        final TextView texrtView = (TextView) v.findViewById(R.id.textViewCounter);
+        Button btnIncrease = (Button) v.findViewById(R.id.btnIncrease);
+        btnIncrease.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                int val = Integer.parseInt(texrtView.getText().toString());
+                val++;
+                texrtView.setText(""+val);
+            }
+        });
+        return v;
     }
 
 }
