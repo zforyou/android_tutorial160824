6cca4b06d5939545770987a716e690c6e2631352
j <zforyou@naver.com>
2016-09-08 21:33:57 +0900

    - first

---
+++ 23_location/app/src/main/java/com/example/b/a23_location/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.b.a23_location;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+
+        LocationManager manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        List<String> providers = manager.getAllProviders();
+        String str = "";
+        for (int i = 0; i < providers.size(); i++) {
+            str += "provider : " + providers.get(i) + " state : " +
+                    manager.isProviderEnabled(providers.get(i)) + "\n";
+        }
+        textView.setText(str);
+        LocationListener listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                String str = "lat : " + location.getLatitude() +
+                        " lon : " + location.getLongitude() + "\n";
+                textView.append(str);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
+        manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0,listener);
+    }
+
+}
