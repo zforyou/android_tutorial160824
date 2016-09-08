1013e340e346822b50c36015ec306efeb6a6bdd8
j <zforyou@naver.com>
2016-09-08 19:26:52 +0900

    - first

---
+++ 21_dialog/app/src/main/java/com/example/b/a21_dialog/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.b.a21_dialog;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_DIALOG = 100;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if( id == MY_DIALOG ){
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("TITLE");
+            builder.setMessage("MESSAGE");
+            builder.setIcon(R.mipmap.ic_launcher);
+
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            return builder.create();
+        }
+        return null;
+    }
+
+    public void onBtnClick(View v){
+        showDialog(MY_DIALOG);
+    }
+}
