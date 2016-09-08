05f13693df19487276b4d7542ecce2e5f122aedd
j <zforyou@naver.com>
2016-09-08 20:58:32 +0900

    -first style button nine patch

--- 21_dialog/app/src/main/java/com/example/b/a21_dialog/MainActivity.java
+++ 21_dialog/app/src/main/java/com/example/b/a21_dialog/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.b.a21_dialog;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Context context;
     private static final int MY_DIALOG = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +29,15 @@ public class MainActivity extends AppCompatActivity {
             builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-                    finish();
+                    Button btn = (Button) findViewById(R.id.btnDialog);
+                    if( btn != null){
+                    btn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            MyPopup myPopup = new MyPopup(MainActivity.this);
+                            myPopup.show();
+                        }
+                    });
                 }
             });
 
@@ -44,8 +54,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onBtnClick(View v){
-//        showDialog(MY_DIALOG);
-        MyPopup myPopup = new MyPopup(MainActivity.this);
-        myPopup.show();
+        showDialog(MY_DIALOG);
+//        MyPopup myPopup = new MyPopup(MainActivity.this);
+//        myPopup.show();
     }
 }
--- 21_dialog/app/src/main/java/com/example/b/a21_dialog/MyPopup.java
+++ 21_dialog/app/src/main/java/com/example/b/a21_dialog/MyPopup.java
@@ -3,8 +3,6 @@ package com.example.b.a21_dialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 /**
  * Created by b on 2016-09-08.
@@ -19,14 +17,14 @@ public class MyPopup extends Dialog {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_layout);
-        Button btn = (Button) findViewById(R.id.btnDialog);
+//        Button btn = (Button) findViewById(R.id.btnDialog);
 //        setCanceledOnTouchOutside(false);
 //        setCancelable(false);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new MyPopup(context).show();
-            }
-        });
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                new MyPopup(context).show();
+//            }
+//        });
     }
 }
